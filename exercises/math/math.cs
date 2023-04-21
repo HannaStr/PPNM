15ca11bad39b2a2e2061e0f32a56573ec71858a2
HannaStr <1913368@gmail.com>
2023-04-21 20:02:43 +0200

    sqrt and sfuns in math

---
+++ exercises/math/math.cs
@@ -0,0 +1,10 @@
+using static System.Math;
+using static System.Console;
+
+class math{
+	static void Main(){
+		//task 1
+		double sqrt2=Sqrt(2.0);
+		Write($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2) \n");
+	}
+}
