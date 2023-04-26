aa590a04d02d7de34ec0be7a466a36118a0e1f66
HannaStr <1913368@gmail.com>
2023-04-26 10:47:33 +0200

    finished exercises math

--- exercises/math/math.cs
+++ exercises/math/math.cs
@@ -1,10 +1,26 @@
+using System;
 using static System.Math;
 using static System.Console;
 
 class math{
 	static void Main(){
-		//task 1
-		double sqrt2=Sqrt(2.0);
+		//square root of 2
+		double sqrt2 = Sqrt(2.0);
+		Write($"sqrt2 = {sqrt2} \n");
 		Write($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2) \n");
+		//2 to the power of 1/5
+		double power2_1fifth = Pow(2.0,0.2);
+		Write($"2^(1/5) = {power2_1fifth} \n");
+		Write($"(2^(1/5))^5 = {Pow(power2_1fifth,5.0)}  (should equal to 2) \n");
+		//e to the power of pi
+		Write($"e^pi = {Pow(E,PI)} \n");
+		//pi to the power of e
+		Write($"pi^e = {Pow(PI,E)} \n");
+
+		//Gamma function
+		double gamma_1 = sfuns.gamma(1.0);
+		double gamma_2 = sfuns.gamma(2.0);
+		double gamma_3 = sfuns.gamma(3.0);
+		Write($"\nGamma function approx:\ngamma(1) = {gamma_1} (should be 1), \ngamma(2) = {gamma_2} (should be 1), \ngamma(3) = {gamma_3} (should be 2),\n");
 	}
 }
--- exercises/math/sfuns.cs
+++ exercises/math/sfuns.cs
@@ -1,12 +1,13 @@
+using System;
 using static System.Math;
 using static System.Console;
 
-class sfuns{
-	static double gamma(double x){
+public static class sfuns{
+	public static double gamma(double x){
 		//single precision gamma function
 		if (x<0) return PI/Sin(PI*x)/gamma(1-x);	//Euler's reflection formula
 		if (x<9) return gamma(x+1)/x;			//Recurrence relation
 		double lngamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
-		return Exp (lngamma);
+		return Exp(lngamma);
 	}
 }
