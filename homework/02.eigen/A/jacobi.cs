bb8ba6e01cc207be2f44edf0321eaa5355fadd1a
HannaStr <1913368@gmail.com>
2023-06-17 21:54:08 +0200

    progress on 02.eigen

--- homework/02.eigen/A/jacobi.cs
+++ homework/02.eigen/A/jacobi.cs
@@ -4,9 +4,9 @@ using static System.Math;
 
 public class jacobi{
 
-    //code from course page part A.1
-    public static void timesJ(matrix A int p, int q, double theta){
-        double c=cos(theta), s=sin(theta);
+    //code from course page part A.1 (can stay as is)
+    public static void timesJ(matrix A, int p, int q, double theta){
+        double c=Cos(theta), s=Sin(theta);
         for(int i=0; i<A.size1; i++){
             double aip=A[i,p], aiq=A[i,q];
             A[i,p]=c*aip-s*aiq;
@@ -14,9 +14,9 @@ public class jacobi{
         }
     }
 
-    //code from course page part A.2
+    //code from course page part A.2 (can stay as is)
     public static void Jtimes(matrix A, int p, int q, double theta){
-        double c=cos(theta), s=sin(theta);
+        double c=Cos(theta), s=Sin(theta);
         for(int j=0; j<A.size1; j++){
             double apj=A[p,j], aqj=A[q,j];
             A[p,j]= c*apj+s*aqj;
@@ -25,25 +25,31 @@ public class jacobi{
     }
 
     //code from course page part A.3
-    public static cyclic(matrix A){
+    public static (matrix,matrix) cyclic(matrix A){                                 //this function requires some changes
+        matrix D = A.copy();
+        int m = A.size1;
+        matrix V = matrix.id(m);
+
         bool changed;
         do{
             changed=false;
-            for(int p=0; p<n-1; p++){
-                for(int q=p+1; q<n; q++){
-                    double apq=A[p,q], app=A[p,p], aqq=A[q,q];      //verify if this is correct
-                    double theta=0.5*Atan2(2*apq, aqq-ap);
-                    double c=cos(theta), s=sin(theta);
+            for(int p=0; p<m-1; p++){
+                for(int q=p+1; q<m; q++){
+                    double apq=D[p,q], app=D[p,p], aqq=D[q,q];      //verify if this is correct
+                    double theta=0.5*Atan2(2*apq, aqq-app);
+                    double c=Cos(theta), s=Sin(theta);
                     double new_app=c*c*app-2*s*c*apq+s*s*aqq;
                     double new_aqq=s*s*app+2*s*c*apq+c*c*aqq;
-                    if(new_app!=app || neq_aqq!=aqq){               //do rotation
+                    if(new_app!=app || new_aqq!=aqq){               //do rotation
                         changed = true;
-                        timesJ(A, p, q, theta);                     // A <-- A * J
-                        Jtimes(A, p, q,-theta);                     // A <-- J^T * A
+                        timesJ(D, p, q, theta);                     // D <-- D * J
+                        Jtimes(D, p, q,-theta);                     // D <-- J^T * D
                         timesJ(V, p, q, theta);                     // V <-- V * J
                     }
                 }
             }
         }
+        while (changed);
+        return (D,V);
     }
 }
--- homework/02.eigen/A/main.cs
+++ homework/02.eigen/A/main.cs
@@ -4,6 +4,38 @@ using static System.Math;
 using static System.Random;
 
 class main{
+    public static void Main(){
+        check_jacobi();
+    }
 
-    
+    public static void check_jacobi(){
+        WriteLine(".................................");
+		WriteLine("TASK: Jacobi eigenvalue algorithm");
+		WriteLine("`````````````````````````````````");
+
+		//generate random symmetric matrix A(n,n)
+		int n=5;
+	
+		matrix A = new matrix(n,n);
+		Random rndm = new Random();
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				A[i,j]=rndm.Next(20);
+                A[i,j]=A[j,i];
+			}
+		}
+
+		WriteLine("\na) Take a random matrix A:");
+		A.print();
+
+        //perform the eigenvalue-decomposition (EVD) ==> A=V*D*(V_transposed)
+        matrix D = A.copy();
+        matrix V = matrix.id(n);
+
+        (D,V)=jacobi.cyclic(A);
+        D.print();
+        V.print();
+
+
+    }
 }
