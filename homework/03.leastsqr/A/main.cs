996879e32f8209f32cbb6fb0b45e44927e62f502
HannaStr <1913368@gmail.com>
2023-06-18 00:39:37 +0200

    started working on 03.leastsqr

---
+++ homework/03.leastsqr/A/main.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+using static System.Math;
+using static System.Random;
+
+class main{
+    public static void Main(){
+        whatever();
+    }
+
+    public static void whatever(){
+
+    }
+}
