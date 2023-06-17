0fd73a3fb41b1a5ea9d82acf0e99d8e9c15f77e5
HannaStr <1913368@gmail.com>
2023-06-18 00:14:11 +0200

    finished 02.eigen - and it works! HURRAYgit add .!

--- homework/02.eigen/A/main.cs
+++ homework/02.eigen/A/main.cs
@@ -21,7 +21,7 @@ class main{
 		for(int i=0; i<n; i++){
 			for(int j=0; j<n; j++){
 				A[i,j]=rndm.Next(20);
-                A[i,j]=A[j,i];
+                A[j,i]=A[i,j];
 			}
 		}
 
@@ -29,13 +29,33 @@ class main{
 		A.print();
 
         //perform the eigenvalue-decomposition (EVD) ==> A=V*D*(V_transposed)
-        matrix D = A.copy();
-        matrix V = matrix.id(n);
+        WriteLine("\nb) Perform the Eigenvalue Decomposition (EVD): A = V * D * V_transposed");
+        matrix D, V;
 
         (D,V)=jacobi.cyclic(A);
+        WriteLine("\n   'D' is a diagonal matrix with the eigenvalues:");
         D.print();
+        WriteLine("\n   and 'V' is a matrix of orthogonalized eigenvectors corresponding to the eigenvalues:");
         V.print();
 
+        //check that V_T*A*V=D
+        matrix Vt_A_V = V.transpose()*A*V;
+        WriteLine("\nc) Calculate (V_transposed * A * V):");
+        Vt_A_V.print();
+        WriteLine($"\n   Note: It should equal to D. In this case it is: {Vt_A_V.approx(D)}");
+        
+        //check that V*D*V_T=A
+        matrix V_D_Vt = V*D*V.transpose();
+        WriteLine("\nd) Calculate (V * D * V_transposed):");
+        V_D_Vt.print();
+        WriteLine($"\n   Note: It should equal to A. In this case it is: {V_D_Vt.approx(A)}");
 
+        //check that V_T*V=I
+        matrix Vt_V = V.transpose()*V;
+        WriteLine($"\ne) Calculate (V_transposed * V)\n   It should be an identity matrix. In this case this is: {Vt_V.approx(matrix.id(n))}");
+
+        //check that V*V_T=I
+        matrix V_Vt = V*V.transpose();
+                WriteLine($"\nf) Calculate (V * V_transposed)\n   It should be an identity matrix. In this case this is: {V_Vt.approx(matrix.id(n))}");
     }
 }
