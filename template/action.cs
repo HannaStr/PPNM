8623340bac6daf7f9c3bb92b51f695b2830bc7e3
HannaStr <1913368@gmail.com>
2023-06-17 19:43:27 +0200

    created templates for solving homeworks

---
+++ template/action.cs
@@ -0,0 +1,9 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class QRGS{
+    public functions_to_solve_the_problem(){
+        
+    }
+}
---
+++ template/main.cs
@@ -0,0 +1,14 @@
+using System;
+using static System.Console;
+using static System.Math;
+using static System.Random;
+
+class main{
+    public static void Main(){
+        functions_I_want_to_run();
+    }
+
+    public static void said_function(){
+        
+    }
+}
