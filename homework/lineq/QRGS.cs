976c0ec34b0003cb19583864e2f2faccf7dbc27f
HannaStr <1913368@gmail.com>
2023-06-17 15:17:02 +0200

    changes after getting help --> decomp => QRGS in QRGS.cs

--- homework/lineq/QRGS.cs
+++ homework/lineq/QRGS.cs
@@ -2,12 +2,12 @@ using System;
 using static System.Console;
 using static System.Math;
 
-public static class QRGS{
+public class QRGS{
     public matrix Q,R;
 
     //Gram-Schmidt orthogonalization of matrix A, calculate R
     //code from notes page 3
-    public static decomp(matrix a){
+    public QRGS (matrix a){
         Q = a.copy();
         int m = a.size2;
         R = new matrix (m,m);
