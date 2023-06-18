618888c03209e74c958f5e4a4903d56a223776e0
HannaStr <1913368@gmail.com>
2023-06-18 23:53:32 +0200

    done the basiscs for 03 part A but in lsfit.cs the QRGS doesn't want to work

--- homework/03.leastsqr/A/main.cs
+++ homework/03.leastsqr/A/main.cs
@@ -5,10 +5,32 @@ using static System.Random;
 
 class main{
     public static void Main(){
-        whatever();
+        lstsqr();
     }
 
-    public static void whatever(){
+    public static void lstsqr(){
+        double [] t  = new double [] {1, 2, 3, 4, 6, 9, 10, 13, 15};
+        double [] y  = new double [] {117, 100, 88, 72, 53, 29.5, 25.2, 15.2, 11.1};
+        double [] dy = new double [] {5, 5, 5, 4, 4, 3, 3, 2, 2};
+
+        var fs = new Func<double,double>[] {z => 1.0, z => z, z => z*z};
+
+        //radioactive decay follows expoential law y(t)=a*exp(-lamda*t)
+        //want to fit ln(y)=ln(a)-lambda*t
+        int n = y.Length;
+        
+        double [] ln_y   = new double [n];
+        double [] d_ln_y = new double [n];
+
+        for(i=0, i<n, i++){
+            ln_y[i] = Log(y[i]);
+            d_ln_y[i] = dy[i]/y[i];
+        }
+
+        vector coeffs = ls_sqr_fit.lsfit(fs, t, ln_y, d_ln_y);
+
+        coeffs.print();
+
 
     }
 }
