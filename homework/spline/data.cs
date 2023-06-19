ee3539536579c6d3ca832ae54d616a510549d099
HannaStr <1913368@gmail.com>
2023-06-19 19:33:37 +0200

    for some reason I can't get Q4 to work

---
+++ homework/spline/data.cs
@@ -0,0 +1,26 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+    public static void Main(){
+        test_lininterp();
+    }
+
+    public static void test_lininterp(){
+        int n = 15;
+        double[] x = new double[n];
+        double[] y = new double[n];
+        for(int j=0; j<n; j++){
+            x[j] = j/2.0;
+            y[j] = Sin(j/2.0);
+        }
+        for(double z=0.0+1/32; z<7; z+=1.0/8){
+            WriteLine($"{z} {spline.linterp(x,y,z)} {spline.linterpInteg(x,y,z)}");
+        }
+        
+
+
+    }
+
+}
