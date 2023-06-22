258db2f0d8b6d8f158e94e7385c4695649319252
HannaStr <1913368@gmail.com>
2023-06-22 21:59:03 +0200

    HW 8 part A mostly done, need to add te rosenbrock's valley function

--- homework/08.roots/A/main.cs
+++ homework/08.roots/A/main.cs
@@ -5,12 +5,34 @@ using static System.Math;
 public class main{
 
     public static void Main(){
-        test_Newton();
+        WriteLine("\nTASK 1: Test the root-finding routine using some simple equations.\n");
+        test_1d();
+        test_2d();
     }
 
+// f(x) = {f_1(x_1, ..., x_n), ..., f_N(x_1, ..., x_n)}
+    public static void test_1d(){
+        //1-dimentional function ==> N=1
+        Func<vector, vector> f = x_n => new vector(Pow(x_n[0],2)-9);
+        vector a_11 = new vector(2.0);
+        vector a_12 = new vector(-0.01);
+        var res_11 = roots.newton(f, a_11);
+        var res_12 = roots.newton(f, a_12);
+        WriteLine("\na) Test for a 1-dimentional function\n");
+        WriteLine("   Find the roots of f(x)=x^2-9:");
+        WriteLine($"   The roots are at: {res_11[0]}  (should be at 3) and at {res_12[0]}  (should be at -3)\n");
+    }
 
-    public static void test_Newton(){
-
+    public static void test_2d(){
+        //2-dimentional function ==> N=2
+        Func<vector, vector> f = x_n => new vector(Pow(x_n[0],2)-9, Pow(x_n[1],2)-4);
+        vector a_21 = new vector( 2.0, 5.0);
+        vector a_22 = new vector(-0.1, -1.0);
+        var res_21 = roots.newton(f, a_21);
+        var res_22 = roots.newton(f, a_22);
+        WriteLine("\nb) Test for a 2-dimentional function\n");
+        WriteLine("   Find the roots of f(x)={(x^2-9), (x^2-4)}:");
+        WriteLine($"   The roots are at: {res_21[0]}, {res_22[0]}  (should be at 3, -3) and at {res_21[1]}, {res_22[1]}  (should be at 2, -2)\n");
     }
 
 }
--- homework/08.roots/A/roots.cs
+++ homework/08.roots/A/roots.cs
@@ -2,57 +2,63 @@ using System;
 using static System.Console;
 using static System.Math;
 
-public static vector newton(
-    Func<vector vector> f,      //takes the input vector x and returns the vector f(x)
-    vector x,                   //the starting point
-    double eps = 1e-2           //the accuracy goal: on exit the condition |f(x)| < eps should be satisfied
-){
 
-/*  EXAMPLE
-    repeat
-        calculate the Jacobian
-        solve J*dx = -f(x) for dx
-        lamda <-- 1
-        while |f(x+lamda*dx)| > (1-lamda/2)|f(x)| and lamda >= 1/1024 do lambda <-- lambda/2
-        x <-- x + lambda*dx
-*/
-    int n = x.size;
-    matrix J = new matrix (n,n);
-    vector f_x = f(x);
-    //double del_x = Sqrt(eps);
+public static class roots{
+    public static vector newton(
+        Func<vector, vector> f,      //takes the input vector x and returns the vector f(x)
+        vector x,                   //the starting point
+        double eps = 1e-2           //the accuracy goal: on exit the condition |f(x)| < eps should be satisfied
+    ){
 
-    //repeat
-    while(diverge){
-        //calculate the Jacobian
-        for(int i=0; i<n; i++){
-            double del_x = Abs(x[i])*Pow(2,-26);  //2^(-26) is the square root of the double (machine epsilon)
-            for(int k=0; k<n; k++){
-                vector x_k = x.copy;
-                x_k[k] += del_x;
-                J[i,k] = (f(x_k)[i]-f_x[i](x))/del_x;
+    /*  EXAMPLE from notes
+        repeat
+            calculate the Jacobian
+            solve J*dx = -f(x) for dx
+            lamda <-- 1
+            while |f(x+lamda*dx)| > (1-lamda/2)|f(x)| and lamda >= 1/1024 do lambda <-- lambda/2
+            x <-- x + lambda*dx
+    */
+        int n = x.size;
+        matrix J = new matrix (n,n);    //Jacobian
+        vector f_x = f(x);
+        bool not_conv = true;           //condition for the function not converging
+        int step_lim = 100;             //how many steps to take before it decides the spot is too difficult
+        //int step = 0.0;
+        //double del_x = Sqrt(eps);
+
+        //repeat
+        while(not_conv){
+            //calculate the Jacobian
+            for(int i=0; i<n; i++){
+                double del_x = Abs(x[i])*Pow(2,-26);  //2^(-26) is the square root of the double (machine epsilon)
+                for(int k=0; k<n; k++){
+                    vector x_k = x.copy();
+                    x_k[k] += del_x;
+                    J[i,k] = (f(x_k)[i] - f(x)[i])/del_x;                //(f(x_k)[i]-f_x[i](x))/del_x;
+                }
             }
+
+            //solve J*dx = -f(x) for dx
+            QRGS lin_system = new QRGS(J);
+            vector dx = lin_system.solve(-f_x);
+
+            //lambda <-- 1
+            double lambda = 1.0;
+
+            //while |f(x+lamda*dx)| > (1-lamda/2)|f(x)| and lamda >= 1/1024 do lambda <-- lambda/2
+            while((f(x+lambda*dx)).norm() > (1-lambda/2)*(f(x)).norm() && lambda >= 1/1024){
+                lambda = lambda/2.0;
+            }
+            //x <-- x + lambda*dx
+            x += lambda*dx;
+            //Stop if either the conditio |f(x)|<eps is satisfied or if the step-size becomes smaller than  the size of the del_x parameter
+            not_conv = (f(x)).norm() > eps && step_lim > 0;
+            f_x = f(x);
+            step_lim--;
         }
 
-        //solve J*dx = -f(x) for dx
-		QRGS lin_system = new QRGS(J);
-		vector dx = lin_system.solve(-f_x);
-		WriteLine("\nb) Solve J*dx= -f(x). \n   Therefore, dx is:");
-		dx.print();
 
-        //lambda <-- 1
-        double lambda = 1.0;
-
-        //while |f(x+lamda*dx)| > (1-lamda/2)|f(x)| and lamda >= 1/1024 do lambda <-- lambda/2
-        while(Abs(f(x+lambda*dx)) > (1-lambda/2)*Abs(f(x)) && lambda >= 1/1024){
-            lambda = lambda/2.0;
-        }
-        //x <-- x + lambda*dx
-        x += lambda*dx;
-        diverge = Abs(f(x)) < eps;
-        f_x = f(x);
+        //dx is the solution to LinEq (J*dx=-f(x)) ==> Newton's step 
+        return x;
     }
-
-
-    //dx is the solution to LinEq (J*dx=-f(x)) ==> Newton's step 
-    return x;
 }
