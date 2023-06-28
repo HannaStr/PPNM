5087f335813c48c4f2f0e9962939781d0620d654
HannaStr <1913368@gmail.com>
2023-06-28 18:43:10 +0200

    exercises genlist

---
+++ exercises/genlist/genlist.cs
@@ -0,0 +1,38 @@
+public class genlist<T>{
+	public T[] data;
+	public int size => data.Length;
+	public int size_double=0, capacity=8;
+	public T this[int i] => data[i];
+	public genlist(){ data = new T[0]; }
+	
+	public void add(T item){
+		T[] newdata = new T[size+1];
+		System.Array.Copy(data,newdata,size);
+		newdata[size]=item;
+		data=newdata;
+	}
+/*
+	public void remove(int i){
+		T[] newdata = new T[size-1];
+		for(int j = 0; j<size; j++){
+			if(j<i) {newdata[j] = data[j];}
+			if(j>i) {newdata[j-1] = data[j];}
+		}
+		data = newdata;
+	}
+*//*	
+	Commented out for ode exercise
+	public genlist(){data = new T[capacity];}
+	public void add_double(T item){ /* add item to list *//*
+		if(size_double==capacity){
+			T[] newdata = new T[capacity*=2];
+			for(int k = 0; k < size_double; k++){
+				newdata[k] = data[k];
+				data=newdata;
+			}
+			}
+		data[size_double]=item;
+		size_double++;
+	}
+	*/
+}
---
+++ exercises/genlist/main.cs
@@ -0,0 +1,59 @@
+using System;
+using static System.Console;
+using static System.Math;
+class main{
+	public static void Main(){
+		genlist<double[]> list = new genlist<double[]>();
+		char[] delimiters = {' ','\t'};
+		var options = StringSplitOptions.RemoveEmptyEntries;
+		for(string line = ReadLine(); line!=null; line = ReadLine()){
+			var words = line.Split(delimiters,options);
+			int n = words.Length;
+			var numbers = new double[n];
+			for(int i=0;i<n;i++) numbers[i] = double.Parse(words[i]);
+			list.add(numbers);
+				}
+		for(int i=0;i<list.size;i++){
+			var numbers = list[i];
+			foreach(var number in numbers)Write($"{number : 0.00e+00;-0.00e+00} ");
+			WriteLine();
+				}
+		
+/*
+		genlist<double> listd = new genlist<double>();
+		listd.add(57.0);
+		listd.add(4.0);
+		listd.add(52.0);
+		listd.add(35.0);
+		listd.add(325.0);
+		WriteLine();
+		WriteLine("Remove the third entry of the list");
+		for(int j=0;j<listd.size;j++){
+			double x = listd[j];
+			WriteLine($"List element {j}: {x} ");
+		}
+		listd.remove(2);
+		for(int k=0; k<listd.size; k++){
+			double y = listd[k];
+			WriteLine($"List with removed 2nd element {k}: {y} ");
+		}
+*/
+//		genlist<double> list_doubled = new genlist<double>();
+//		list_doubled.add_double(24.0);
+		/*
+		list_doubled.add_double(236.0);
+		list_doubled.add_double(26.0);
+		list_doubled.add_double(234.0);
+		list_doubled.add_double(2346.0);
+		list_doubled.add_double(4.0);
+		list_doubled.add_double(6.0);
+		list_doubled.add_double(644.0);
+		list_doubled.add_double(346.0);
+		list_doubled.add_double(246.0);
+		
+		for(int l=0;l<list_doubled.size_double;l++){
+			double x = list_doubled[l];
+			WriteLine($"List element {l}: {x} ");
+		}*/
+	}
+}
